b5c75d8e661adb338b08ca1d7f5636948ed7a05c
mhtraore <mhtraore@certesmali.org>
2026-04-20 13:22:22 +0000

    update

--- resources/views/report/product_sell_report.blade.php
+++ resources/views/report/product_sell_report.blade.php
@@ -13,77 +13,87 @@
     <div class="row">
         <div class="col-md-12">
             @component('components.filters', ['title' => __('report.filters')])
-              {!! Form::open(['url' => action([\App\Http\Controllers\ReportController::class, 'getStockReport']), 'method' => 'get', 'id' => 'product_sell_report_form' ]) !!}
-                <div class="col-md-3">
-                    <div class="form-group">
+            {!! Form::open(['url' => action([\App\Http\Controllers\ReportController::class, 'getStockReport']), 'method'
+            => 'get', 'id' => 'product_sell_report_form' ]) !!}
+            <div class="col-md-3">
+                <div class="form-group">
                     {!! Form::label('search_product', __('lang_v1.search_product') . ':') !!}
-                        <div class="input-group">
-                            <span class="input-group-addon">
-                                <i class="fa fa-search"></i>
-                            </span>
-                            <input type="hidden" value="" id="variation_id">
-                            {!! Form::text('search_product', null, ['class' => 'form-control', 'id' => 'search_product', 'placeholder' => __('lang_v1.search_product_placeholder'), 'autofocus']); !!}
-                        </div>
+                    <div class="input-group">
+                        <span class="input-group-addon">
+                            <i class="fa fa-search"></i>
+                        </span>
+                        <input type="hidden" value="" id="variation_id">
+                        {!! Form::text('search_product', null, ['class' => 'form-control', 'id' => 'search_product',
+                        'placeholder' => __('lang_v1.search_product_placeholder'), 'autofocus']); !!}
                     </div>
                 </div>
-                <div class="col-md-3">
-                    <div class="form-group">
-                        {!! Form::label('customer_id', __('contact.customer') . ':') !!}
-                        <div class="input-group">
-                            <span class="input-group-addon">
-                                <i class="fa fa-user"></i>
-                            </span>
-                            {!! Form::select('customer_id', $customers, null, ['class' => 'form-control select2', 'placeholder' => __('messages.please_select'), 'required', 'style' => 'width:100%']); !!}
-                        </div>
+            </div>
+            <div class="col-md-3">
+                <div class="form-group">
+                    {!! Form::label('customer_id', __('contact.customer') . ':') !!}
+                    <div class="input-group">
+                        <span class="input-group-addon">
+                            <i class="fa fa-user"></i>
+                        </span>
+                        {!! Form::select('customer_id', $customers, null, ['class' => 'form-control select2',
+                        'placeholder' => __('messages.please_select'), 'required', 'style' => 'width:100%']); !!}
                     </div>
                 </div>
-                <div class="col-md-3">
-                    <div class="form-group">
-                        {!! Form::label('psr_customer_group_id', __( 'lang_v1.customer_group_name' ) . ':') !!}
-                        {!! Form::select('psr_customer_group_id', $customer_group, null, ['class' => 'form-control select2', 'style' => 'width:100%', 'id' => 'psr_customer_group_id']); !!}
+            </div>
+            <div class="col-md-3">
+                <div class="form-group">
+                    {!! Form::label('psr_customer_group_id', __( 'lang_v1.customer_group_name' ) . ':') !!}
+                    {!! Form::select('psr_customer_group_id', $customer_group, null, ['class' => 'form-control select2',
+                    'style' => 'width:100%', 'id' => 'psr_customer_group_id']); !!}
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="form-group">
+                    {!! Form::label('location_id', __('purchase.business_location').':') !!}
+                    <div class="input-group">
+                        <span class="input-group-addon">
+                            <i class="fa fa-map-marker"></i>
+                        </span>
+                        {!! Form::select('location_id', $business_locations, null, ['class' => 'form-control select2',
+                        'placeholder' => __('messages.please_select'), 'required', 'style' => 'width:100%']); !!}
                     </div>
                 </div>
-                <div class="col-md-3">
-                    <div class="form-group">
-                        {!! Form::label('location_id', __('purchase.business_location').':') !!}
-                        <div class="input-group">
-                            <span class="input-group-addon">
-                                <i class="fa fa-map-marker"></i>
-                            </span>
-                            {!! Form::select('location_id', $business_locations, null, ['class' => 'form-control select2', 'placeholder' => __('messages.please_select'), 'required', 'style' => 'width:100%']); !!}
-                        </div>
-                    </div>
+            </div>
+            <div class="col-md-3">
+                <div class="form-group">
+                    {!! Form::label('category_id', __('product.category') . ':') !!}
+                    {!! Form::select('category_id', $categories, null, ['class' => 'form-control select2', 'style' =>
+                    'width:100%', 'id' => 'psr_filter_category_id', 'placeholder' => __('lang_v1.all')]); !!}
                 </div>
-                <div class="col-md-3">
-                    <div class="form-group">
-                        {!! Form::label('category_id', __('product.category') . ':') !!}
-                        {!! Form::select('category_id', $categories, null, ['class' => 'form-control select2', 'style' => 'width:100%', 'id' => 'psr_filter_category_id', 'placeholder' => __('lang_v1.all')]); !!}
-                    </div>
+            </div>
+            <div class="col-md-3">
+                <div class="form-group">
+                    {!! Form::label('brand_id', __('product.brand') . ':') !!}
+                    {!! Form::select('brand_id', $brands, null, ['class' => 'form-control select2', 'style' =>
+                    'width:100%', 'id' => 'psr_filter_brand_id', 'placeholder' => __('lang_v1.all')]); !!}
                 </div>
-                <div class="col-md-3">
-                    <div class="form-group">
-                        {!! Form::label('brand_id', __('product.brand') . ':') !!}
-                        {!! Form::select('brand_id', $brands, null, ['class' => 'form-control select2', 'style' => 'width:100%', 'id' => 'psr_filter_brand_id', 'placeholder' => __('lang_v1.all')]); !!}
-                    </div>
+            </div>
+            <div class="col-md-3">
+                <div class="form-group">
+                    {!! Form::label('product_sr_date_filter', __('report.date_range') . ':') !!}
+                    {!! Form::text('date_range', null, ['placeholder' => __('lang_v1.select_a_date_range'), 'class' =>
+                    'form-control', 'id' => 'product_sr_date_filter', 'readonly']); !!}
                 </div>
-                <div class="col-md-3">
-                    <div class="form-group">
-                        {!! Form::label('product_sr_date_filter', __('report.date_range') . ':') !!}
-                        {!! Form::text('date_range', null, ['placeholder' => __('lang_v1.select_a_date_range'), 'class' => 'form-control', 'id' => 'product_sr_date_filter', 'readonly']); !!}
-                    </div>
+            </div>
+            <div class="col-md-3">
+                {!! Form::label('product_sr_start_time', __('lang_v1.time_range') . ':') !!}
+                @php
+                $startDay ='00:00:00';// Carbon::now()->startOfDay();
+                $endDay = '23:59:59';// $startDay->copy()->endOfDay();
+                @endphp
+                <div class="form-group">
+                    {!! Form::text('start_time', @format_time($startDay), ['style' => __('lang_v1.select_a_date_range'),
+                    'class' => 'form-control width-50 f-left', 'id' => 'product_sr_start_time']); !!}
+                    {!! Form::text('end_time', @format_time($endDay), ['class' => 'form-control width-50 f-left', 'id'
+                    => 'product_sr_end_time']); !!}
                 </div>
-                <div class="col-md-3">
-                    {!! Form::label('product_sr_start_time', __('lang_v1.time_range') . ':') !!}
-                    @php
-                        $startDay = Carbon::now()->startOfDay();
-                        $endDay   = $startDay->copy()->endOfDay();
-                    @endphp
-                    <div class="form-group">
-                        {!! Form::text('start_time', @format_time($startDay), ['style' => __('lang_v1.select_a_date_range'), 'class' => 'form-control width-50 f-left', 'id' => 'product_sr_start_time']); !!}
-                        {!! Form::text('end_time', @format_time($endDay), ['class' => 'form-control width-50 f-left', 'id' => 'product_sr_end_time']); !!}
-                    </div>
-                </div>
-                {!! Form::close() !!}
+            </div>
+            {!! Form::close() !!}
             @endcomponent
         </div>
     </div>
@@ -92,26 +102,30 @@
             <div class="nav-tabs-custom">
                 <ul class="nav nav-tabs">
                     <li class="active">
-                        <a href="#psr_detailed_tab" data-toggle="tab" aria-expanded="true"><i class="fa fa-list" aria-hidden="true"></i> @lang('lang_v1.detailed')</a>
+                        <a href="#psr_detailed_tab" data-toggle="tab" aria-expanded="true"><i class="fa fa-list"
+                                aria-hidden="true"></i> @lang('lang_v1.detailed')</a>
                     </li>
                     <li>
-                        <a href="#psr_detailed_with_purchase_tab" data-toggle="tab" aria-expanded="true"><i class="fa fa-list" aria-hidden="true"></i> @lang('lang_v1.detailed_with_purchase')</a>
+                        <a href="#psr_detailed_with_purchase_tab" data-toggle="tab" aria-expanded="true"><i
+                                class="fa fa-list" aria-hidden="true"></i> @lang('lang_v1.detailed_with_purchase')</a>
                     </li>
                     <li>
-                        <a href="#psr_grouped_tab" data-toggle="tab" aria-expanded="true"><i class="fa fa-bars" aria-hidden="true"></i> @lang('lang_v1.grouped')</a>
+                        <a href="#psr_grouped_tab" data-toggle="tab" aria-expanded="true"><i class="fa fa-bars"
+                                aria-hidden="true"></i> @lang('lang_v1.grouped')</a>
                     </li>
                     <li>
-                        <a href="#psr_by_cat_tab" data-toggle="tab" aria-expanded="true"><i class="fa fa-bars" aria-hidden="true"></i> @lang('lang_v1.by_category')</a>
+                        <a href="#psr_by_cat_tab" data-toggle="tab" aria-expanded="true"><i class="fa fa-bars"
+                                aria-hidden="true"></i> @lang('lang_v1.by_category')</a>
                     </li>
                     <li>
-                        <a href="#psr_by_brand_tab" data-toggle="tab" aria-expanded="true"><i class="fa fa-bars" aria-hidden="true"></i> @lang('lang_v1.by_brand')</a>
+                        <a href="#psr_by_brand_tab" data-toggle="tab" aria-expanded="true"><i class="fa fa-bars"
+                                aria-hidden="true"></i> @lang('lang_v1.by_brand')</a>
                     </li>
                 </ul>
                 <div class="tab-content">
                     <div class="tab-pane active" id="psr_detailed_tab">
                         <div class="table-responsive">
-                            <table class="table table-bordered table-striped" 
-                            id="product_sell_report_table">
+                            <table class="table table-bordered table-striped" id="product_sell_report_table">
                                 <thead>
                                     <tr>
                                         <th>@lang('sale.product')</th>
@@ -141,7 +155,8 @@
                                         <td></td>
                                         <td id="footer_tax"></td>
                                         <td></td>
-                                        <td><span class="display_currency" id="footer_subtotal" data-currency_symbol ="true"></span></td>
+                                        <td><span class="display_currency" id="footer_subtotal"
+                                                data-currency_symbol="true"></span></td>
                                         <td></td>
                                     </tr>
                                 </tfoot>
@@ -151,10 +166,10 @@
                     <div class="tab-pane" id="psr_detailed_with_purchase_tab">
                         <div class="table-responsive">
                             @if(session('business.enable_lot_number'))
-                                <input type="hidden" id="lot_enabled">
+                            <input type="hidden" id="lot_enabled">
                             @endif
-                            <table class="table table-bordered table-striped" 
-                            id="product_sell_report_with_purchase_table" style="width: 100%;">
+                            <table class="table table-bordered table-striped"
+                                id="product_sell_report_with_purchase_table" style="width: 100%;">
                                 <thead>
                                     <tr>
                                         <th>@lang('sale.product')</th>
@@ -176,8 +191,8 @@
 
                     <div class="tab-pane" id="psr_grouped_tab">
                         <div class="table-responsive">
-                            <table class="table table-bordered table-striped" 
-                            id="product_sell_grouped_report_table" style="width: 100%;">
+                            <table class="table table-bordered table-striped" id="product_sell_grouped_report_table"
+                                style="width: 100%;">
                                 <thead>
                                     <tr>
                                         <th>@lang('sale.product')</th>
@@ -192,7 +207,8 @@
                                     <tr class="bg-gray font-17 footer-total text-center">
                                         <td colspan="4"><strong>@lang('sale.total'):</strong></td>
                                         <td id="footer_total_grouped_sold"></td>
-                                        <td><span class="display_currency" id="footer_grouped_subtotal" data-currency_symbol ="true"></span></td>
+                                        <td><span class="display_currency" id="footer_grouped_subtotal"
+                                                data-currency_symbol="true"></span></td>
                                     </tr>
                                 </tfoot>
                             </table>
@@ -207,16 +223,15 @@
     </div>
 </section>
 <!-- /.content -->
-<div class="modal fade view_register" tabindex="-1" role="dialog" 
-    aria-labelledby="gridSystemModalLabel">
+<div class="modal fade view_register" tabindex="-1" role="dialog" aria-labelledby="gridSystemModalLabel">
 </div>
 
 @endsection
 
 @section('javascript')
-    <script src="{{ asset('js/report.js?v=' . $asset_v) }}"></script>
-    <script type="text/javascript">
-        $(
+<script src="{{ asset('js/report.js?v=' . $asset_v) }}"></script>
+<script type="text/javascript">
+    $(
         '#product_sell_report_form #location_id, #product_sell_report_form #customer_id, #psr_filter_brand_id, #psr_filter_category_id, #psr_customer_group_id'
     ).change(function() {
         $('.nav-tabs li.active').find('a[data-toggle="tab"]').trigger('shown.bs.tab');
@@ -341,5 +356,5 @@
                     }
                 });
             });
-    </script>
+</script>
 @endsection
